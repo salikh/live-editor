50b60ae683acb8b908a0139aed4b8e42edf86723
Salikh Zakirov <salikh@google.com>
2021-01-05 20:52:17 +0900

    loadNamed

--- build/js/live-editor.ui.js
+++ build/js/live-editor.ui.js
@@ -2335,14 +2335,16 @@ if (typeof exports !== "undefined") {
 
 // Malformed JSON, we don't care about it
 var data = {
+  name: null,
   help_on: false,
   message: null,
-  name: null,
+  fullname: null,
   uid: null,
-  original_code: window.localStorage['test-code']
+  original_code: window.localStorage['test-code'],
+  original_name: null,
+  original_uid: null,
+  name_error: false
 };
-
-function updateRegularly() {}
 
 function parseFragment() {
   var params = {};
@@ -2351,8 +2353,8 @@ function parseFragment() {
     var part = parts[i];
     if (part == '') continue;
     var pieces = part.split('=');
-    if (pieces.length != 2) {
-      window.console.error('Invalid fragment parameter: ' + part);
+    if (pieces.length == 1) {
+      params[''] = decodeURIComponent(part);
       continue;
     }
     params[pieces[0]] = decodeURIComponent(pieces[1]);
@@ -2363,6 +2365,7 @@ function parseFragment() {
 /**
  * Update the fragment hash with a new key-value pair.
  * If value is null, removes that part from the fragment hash.
+ * If key is empty string, sets the hash to that value.
  * @param {string} key
  * @param {?string} value
  */
@@ -2378,7 +2381,11 @@ function updateFragment(key, value) {
     if (hash.length > 0) {
       hash = hash + '&';
     }
-    hash = hash + encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+    if (key != '') {
+      hash = hash + encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+    } else {
+      hash = hash + encodeURIComponent(params[key]);
+    }
   }
   window.location.hash = hash;
 }
@@ -2395,6 +2402,66 @@ function generateBase62ID(numchars) {
   return ret;
 }
 
+function loadNamed(name) {
+  firebase.database().ref('/shared/' + name).once('value').then(function (snapshot) {
+    if (snapshot.val() === null) {
+      window.console.error('did not find ' + id);
+      return;
+    }
+    window.liveEditor.editor.text(snapshot.val().code);
+    data.original_code = snapshot.val().code;
+    data.original_name = name;
+    data.original_uid = snapshot.val().uid;
+    if (snapshot.val().uid == data.uid) {
+      data.name = name;
+    }
+  })['catch'](function (error) {
+    window.console.error('could not load named program ' + name, error);
+  });
+}
+
+function clearMessage() {
+  Vue.set(data, 'message', null);
+}
+
+/** Stores the source code by name */
+function saveNamed(name, code) {
+  clearMessage();
+  // Should not happen as 'Save' button is disabled in UI.
+  if (firebase.auth().currentUser.uid == null) {
+    window.console.error('Not logged in');
+    return;
+  }
+  firebase.database().ref('/shared/' + name).once('value').then(function (snapshot) {
+    if (snapshot.val() != null && snapshot.val().uid != data.uid) {
+      data.message = 'Name ' + name + ' is already taken';
+      window.console.error('Name ' + name + ' is already taken');
+      data.name_error = true;
+      return;
+    }
+    var updates = {};
+    updates['/shared/' + name] = {
+      code: code,
+      time: firebase.database.ServerValue.TIMESTAMP,
+      uid: firebase.auth().currentUser.uid
+    };
+    firebase.database().ref().update(updates).then(function () {
+      updateFragment('load', null);
+      updateFragment('id', null);
+      updateFragment('', name);
+      data.original_code = code;
+      data.original_name = name;
+      data.original_uid = firebase.auth().currentUser.uid;
+      data.name_error = false;
+      window.console.log('Saved named: ' + name);
+    })['catch'](function (error) {
+      window.console.error('Could not save ' + name, error);
+      data.message = 'Unable to save project: ' + error;
+    });
+  });
+}
+
+/** Loads the source code by id */
 function loadSource(id) {
   firebase.database().ref('/code/' + id).once('value').then(function (snapshot) {
     if (snapshot.val() === null) {
@@ -2408,9 +2475,9 @@ function loadSource(id) {
   });
 }
 
+/** Stores the source code by id */
 function saveSource(id, code) {
   var updates = {};
-  var revisionId = firebase.database().ref().child('/code/' + name).push().key;
   updates['/code/' + id] = {
     code: code,
     time: firebase.database.ServerValue.TIMESTAMP,
@@ -2418,13 +2485,48 @@ function saveSource(id, code) {
   };
   firebase.database().ref().update(updates).then(function () {
     updateFragment('load', null);
+    updateFragment('', null);
     updateFragment('id', id);
     data.original_code = code;
-    window.console.log('saved ' + id);
+    data.original_name = null;
+    data.original_uid = null;
+    window.console.log('Saved ' + id);
   })['catch'](function (error) {
     window.console.error('could not save ' + id, error);
     data.message = 'Unable to save project: ' + error;
   });
+}
+
+function save() {
+  var code = window.liveEditor.editor.text();
+  if (code == data.original_code && (data.name == data.original_name || !data.name)) {
+    window.console.log('no changes');
+    return;
+  }
+  var params = parseFragment();
+  if (data.name) {
+    saveNamed(data.name, code);
+  } else if ('' in params && data.original_uid == data.uid) {
+    data.name = params[''];
+    saveNamed(data.name, code);
+  } else {
+    window.console.log('original_uid', data.original_uid, 'uid', data.uid);
+    var _id = undefined;
+    if ('id' in params) {
+      _id = params['id'];
+    } else {
+      _id = generateBase62ID(5);
+    }
+    saveSource(_id, code);
+  }
+}
+
+function updateRegularly() {
+  var code = window.liveEditor.editor.text();
+  if (code == data.original_code && (data.name == data.original_name || !data.name)) {
+    return;
+  }
+  save();
 }
 
 window.addEventListener('load', function () {
@@ -2435,21 +2537,20 @@ window.addEventListener('load', function () {
       help: function help(help_on) {
         data.help_on = help_on;
       },
-      save: function save() {
-        var code = window.liveEditor.editor.text();
-        if (code == data.original_code) {
-          window.console.log('no changes');
-          return;
-        }
-        var params = parseFragment();
-        var id = undefined;
-        if ('id' in params) {
-          id = params['id'];
-        } else {
-          id = generateBase62ID(5);
-          updateFragment('id', id);
-        }
-        saveSource(id, code);
+      name_change: function name_change() {
+        data.name_error = false;
+      },
+      save: save,
+      new_program: function new_program() {
+        updateFragment('id', null);
+        updateFragment('load', null);
+        updateFragment('', null);
+        var code = 'rect(10, 20, 200, 300);';
+        window.liveEditor.editor.text(code);
+        data.original_code = code;
+        data.original_name = null;
+        data.original_uid = null;
+        data.name = null;
       },
       clear_message: function clear_message() {
         data.message = null;
@@ -2462,35 +2563,60 @@ window.addEventListener('load', function () {
           //var token = result.credential.accessToken;
           // The signed-in user info.
           window.console.log(result.user);
-          data.name = result.user.displayName;
+          data.fullname = result.user.displayName;
           data.uid = result.user.uid;
+          if (data.original_uid == data.uid && data.original_name) {
+            data.name = data.original_name;
+          }
         })['catch'](function (error) {
           window.console.log(error);
           data.message = 'Error logging in: ' + error.message;
-          data.name = null;
+          data.fullname = null;
           data.uid = null;
         });
       },
       logout: function logout() {
         firebase.auth().signOut().then(function () {
-          data.name = null;
+          data.fullname = null;
           data.uid = null;
         })['catch'](function (error) {
           window.console.log(error);
           data.message = 'Error logging out: ' + error.message;
-          data.name = null;
+          data.fullname = null;
           data.uid = null;
         });
       }
     }
   });
   var params = parseFragment();
-  if ('id' in params) {
+  if ('' in params) {
+    loadNamed(params['']);
+    if ('id' in params) updateFragment('id', null);
+    if ('load' in params) updateFragment('load', null);
+  } else if ('id' in params) {
     loadSource(params['id']);
+    if ('load' in params) updateFragment('load', null);
   } else if ('load' in params) {
-    loadSource(params['id']);
+    loadSource(params['load']);
   }
   window.setInterval(updateRegularly, 5000);
+  /*
+  // TODO(salikh): Implement help on hover.
+  window.liveEditor.editor.editor.on('mousemove', function(e) {
+    let editor = window.liveEditor.editor.editor;
+    let position = e.getDocumentPosition();
+    var token = editor.session.getTokenAt(position.row, position.column);
+    if (token != null) {
+      window.console.log('token under mouse: ', token.value, e);
+    }
+    //Vue.set(data, 'message', token.value);
+  });
+  */
+  fetch('docs.html').then(function (response) {
+    return response.text();
+  }).then(function (html) {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(html, 'text/html');
+    window.console.log(doc);
+  });
 });
-
-// TODO(salikh): Add autosave.
--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 var data = {
+  name: null,
   help_on: false,
   message: null,
-  name: null,
+  fullname: null,
   uid: null,
   original_code: window.localStorage["test-code"],
+  original_name: null,
+  original_uid: null,
+  name_error: false,
 };
-
-function updateRegularly() {
-  // TODO(salikh): Add autosave.
-}
 
 function parseFragment() {
   var params = {};
@@ -17,8 +17,8 @@ function parseFragment() {
     var part = parts[i];
     if (part == '') continue;
     var pieces = part.split('=');
-    if (pieces.length != 2) {
-      window.console.error('Invalid fragment parameter: ' + part);
+    if (pieces.length == 1) {
+      params[''] = decodeURIComponent(part);
       continue;
     }
     params[pieces[0]] = decodeURIComponent(pieces[1]);
@@ -29,6 +29,7 @@ function parseFragment() {
 /**
  * Update the fragment hash with a new key-value pair.
  * If value is null, removes that part from the fragment hash.
+ * If key is empty string, sets the hash to that value.
  * @param {string} key
  * @param {?string} value
  */
@@ -44,7 +45,11 @@ function updateFragment(key, value) {
     if (hash.length > 0) {
       hash = hash + '&';
     }
-    hash = hash + encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+    if (key != "") {
+      hash = hash + encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+    } else {
+      hash = hash + encodeURIComponent(params[key]);
+    }
   }
   window.location.hash = hash;
 }
@@ -61,6 +66,67 @@ function generateBase62ID(numchars) {
   return ret;
 }
 
+function loadNamed(name) {
+  firebase.database().ref('/shared/' + name).once('value').then(function(snapshot) {
+    if (snapshot.val() === null) {
+      window.console.error('did not find ' + id);
+      return;
+    }
+    window.liveEditor.editor.text(snapshot.val().code);
+    data.original_code = snapshot.val().code;
+    data.original_name = name;
+    data.original_uid = snapshot.val().uid;
+    if (snapshot.val().uid == data.uid) {
+      data.name = name;
+    }
+  }).catch(function(error) {
+    window.console.error('could not load named program ' + name, error);
+  })
+}
+
+function clearMessage() {
+  Vue.set(data, 'message', null);
+}
+
+/** Stores the source code by name */
+function saveNamed(name, code) {
+  clearMessage();
+  // Should not happen as 'Save' button is disabled in UI.
+  if (firebase.auth().currentUser.uid == null) {
+    window.console.error('Not logged in');
+    return; 
+  }
+  firebase.database().ref('/shared/' + name).once('value').then(function(snapshot) {
+    if (snapshot.val() != null && snapshot.val().uid != data.uid) {
+      data.message = 'Name ' + name + ' is already taken';
+      window.console.error('Name ' + name + ' is already taken');
+      data.name_error = true;
+      return;
+    }
+    var updates = {} 
+    updates["/shared/" + name] = {
+      code: code,
+      time: firebase.database.ServerValue.TIMESTAMP,
+      uid: firebase.auth().currentUser.uid
+    };
+    firebase.database().ref().update(updates).then(function(){
+      updateFragment('load', null);
+      updateFragment('id', null);
+      updateFragment('', name);
+      data.original_code = code;
+      data.original_name = name;
+      data.original_uid = firebase.auth().currentUser.uid;
+      data.name_error = false;
+      window.console.log('Saved named: ' + name);
+    }).catch(function(error){
+      window.console.error('Could not save ' + name, error);
+      data.message = "Unable to save project: " + error;
+    });
+  });
+}
+
+
+/** Loads the source code by id */
 function loadSource(id) {
   firebase.database().ref('/code/' + id ).once('value').then(function(snapshot) {
     if (snapshot.val() === null) {
@@ -74,9 +140,9 @@ function loadSource(id) {
   })
 }
 
+/** Stores the source code by id */
 function saveSource(id, code) {
   var updates = {} 
-  var revisionId = firebase.database().ref().child("/code/" + name).push().key
   updates["/code/" + id] = {
     code: code,
     time: firebase.database.ServerValue.TIMESTAMP,
@@ -84,13 +150,48 @@ function saveSource(id, code) {
   };
   firebase.database().ref().update(updates).then(function(){
     updateFragment('load', null);
+    updateFragment('', null);
     updateFragment('id', id);
     data.original_code = code;
-    window.console.log('saved ' + id);
+    data.original_name = null;
+    data.original_uid = null;
+    window.console.log('Saved ' + id);
   }).catch(function(error){
     window.console.error('could not save ' + id, error);
     data.message = "Unable to save project: " + error;
   });
+}
+
+function save() {
+  let code = window.liveEditor.editor.text();
+  if (code == data.original_code && (data.name == data.original_name || !data.name)) {
+    window.console.log('no changes');
+    return;
+  }
+  let params = parseFragment();
+  if (data.name) {
+    saveNamed(data.name, code);
+  } else if ('' in params && data.original_uid == data.uid) {
+    data.name = params[''];
+    saveNamed(data.name, code);
+  } else {
+    window.console.log('original_uid', data.original_uid, 'uid', data.uid);
+    let id;
+    if ('id' in params) {
+      id = params['id'];
+    } else {
+      id = generateBase62ID(5);
+    }
+    saveSource(id, code);
+  }
+}
+
+function updateRegularly() {
+  let code = window.liveEditor.editor.text();
+  if (code == data.original_code && (data.name == data.original_name || !data.name)) {
+    return;
+  }
+  save();
 }
 
 window.addEventListener('load', function() {
@@ -101,21 +202,20 @@ window.addEventListener('load', function() {
       help: function(help_on) {
 	data.help_on = help_on;
       },
-      save: function() {
-        let code = window.liveEditor.editor.text();
-	if (code == data.original_code) {
-	  window.console.log('no changes');
-	  return;
-	}
-	let params = parseFragment();
-	let id;
-	if ('id' in params) {
-	  id = params['id'];
-	} else {
-	  id = generateBase62ID(5);
-	  updateFragment('id', id);
-	}
-	saveSource(id, code);
+      name_change: function() {
+	data.name_error = false;
+      },
+      save: save,
+      new_program: function() {
+	updateFragment('id', null);
+	updateFragment('load', null);
+	updateFragment('', null);
+	let code = 'rect(10, 20, 200, 300);'
+	window.liveEditor.editor.text(code);
+	data.original_code = code;
+	data.original_name = null;
+	data.original_uid = null;
+	data.name = null;
       },
       clear_message: function() {
 	data.message = null;
@@ -128,34 +228,61 @@ window.addEventListener('load', function() {
 	  //var token = result.credential.accessToken;
 	  // The signed-in user info.
 	  window.console.log(result.user);
-	  data.name = result.user.displayName;
+	  data.fullname = result.user.displayName;
 	  data.uid = result.user.uid;
+	  if (data.original_uid == data.uid && data.original_name) {
+	    data.name = data.original_name;
+	  }
 	}).catch(function(error) {
 	  window.console.log(error);
 	  data.message = 'Error logging in: ' + error.message;
-	  data.name = null;
+	  data.fullname = null;
 	  data.uid = null;
 	});
 
       },
       logout: function() {
 	firebase.auth().signOut().then(() => {
-	  data.name = null;
+	  data.fullname = null;
 	  data.uid = null;
 	}).catch((error) => {
 	  window.console.log(error);
 	  data.message = 'Error logging out: ' + error.message;
-	  data.name = null;
+	  data.fullname = null;
 	  data.uid = null;
 	});
       },
     },
   });
   let params = parseFragment();
-  if ('id' in params) {
+  if ('' in params) {
+    loadNamed(params['']);
+    if ('id' in params) updateFragment('id', null);
+    if ('load' in params) updateFragment('load', null);
+  } else if ('id' in params) {
     loadSource(params['id']);
+    if ('load' in params) updateFragment('load', null);
   } else if ('load' in params) {
-    loadSource(params['id']);
+    loadSource(params['load']);
   }
   window.setInterval(updateRegularly, 5000);
+  /*
+  // TODO(salikh): Implement help on hover.
+  window.liveEditor.editor.editor.on('mousemove', function(e) {
+    let editor = window.liveEditor.editor.editor;
+    let position = e.getDocumentPosition();
+    var token = editor.session.getTokenAt(position.row, position.column);
+    if (token != null) {
+      window.console.log('token under mouse: ', token.value, e);
+    }
+    //Vue.set(data, 'message', token.value);
+  });
+  */
+  fetch('docs.html')
+    .then(response => response.text()) 
+    .then(html => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+      window.console.log(doc);
+    });
 })
